439f595bfe7490faebf167791530b1830a0ae70b
Toni Hermoso Pulido <toniher@cau.cat>
2018-11-28 17:09:36 +0100

    run pipeline mysql

---
+++ run_pipeline_mysql.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+# Wrapper for running the pipeline in MySQL mode - Use with nohup and ideally save log
+
+# Parsing params.config (the same place as nexflow for sake of simplicity)
+
+# If MySQL mode
+
+# Check all MySQL params are there
+
+# If so
+
+# Run MySQL qsub process
+
+# Run Nextflow pipeline
+
+# Else, stop process
+
+# Else, SQLite mode
+
+# Run Nextflow pipeline
