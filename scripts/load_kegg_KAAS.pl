f747d892bd5a870e0528fa6c1ce7b5f5f4177005
Toni Hermoso Pulido <toniher@cau.cat>
2021-03-23 19:36:12 +0100

    fix stupid problem

--- scripts/load_kegg_KAAS.pl
+++ scripts/load_kegg_KAAS.pl
@@ -471,7 +471,7 @@ sub uploadKeggInformation {
 			if ($#orthobucket >= 0) {
 				my $query;
 				# VALUES here used for replacement
-				if ( lc($dbEngine eq 'sqlite') ) {
+				if ( lc($dbEngine) eq 'sqlite' ) {
 					$query = "INSERT INTO ortholog (name, organism_id, db_id, db_name) VALUES #VALUES# ON CONFLICT DO UPDATE SET name = values(name), organism_id = values(organism_id), db_id = values(db_id) ;";
 				} else {
 					$query = "INSERT INTO ortholog (name, organism_id, db_id, db_name) VALUES #VALUES# ON DUPLICATE KEY UPDATE name = values(name), organism_id = values(organism_id), db_id = values(db_id) ;";
@@ -567,7 +567,7 @@ sub uploadKeggInformation {
 			if ($#porthobucket >= 0) {
 				my $query;
 				# VALUES here used for replacement
-				if ( lc($dbEngine eq 'sqlite') ) {
+				if ( lc($dbEngine) eq 'sqlite' ) {
 					$query = "INSERT INTO protein_ortholog (protein_id, ortholog_id, type, kegg_group_id) VALUES #VALUES# ON CONFLICT DO UPDATE SET protein_id=values(protein_id), ortholog_id=values(ortholog_id), type=values(ortholog_id), kegg_group_id=values(kegg_group_id) ;";
 				} else {
 					$query = "INSERT INTO protein_ortholog (protein_id, ortholog_id, type, kegg_group_id) VALUES #VALUES# ON DUPLICATE KEY UPDATE protein_id=values(protein_id), ortholog_id=values(ortholog_id), type=values(ortholog_id), kegg_group_id=values(kegg_group_id) ;";
